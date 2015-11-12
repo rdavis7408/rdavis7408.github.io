6be5cbb96149ac0a81a22e688cbb7944f97f7371
rdavis7408 <rdavis7408@gmail.com>
2015-11-12 09:35:22 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,7 @@
-$("#main").append("Robert Davis");
+var name = "Robert L. Davis"
+var role = "Business Analyst"
+var formattedName = HTMLheaderName.replace("%data",name)
+var formattedRole = HTMLheaderRole.replace("data", role)
+$("#header").append(formattedName);
+$("#header").prepend(formattedRole);
 
