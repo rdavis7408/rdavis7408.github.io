91c507339afbe15c81151f820d8842624ab70f36
rdavis7408 <rdavis7408@gmail.com>
2015-11-13 15:19:35 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,3 +5,12 @@ var formattedRole = HTMLheaderRole.replace("%data%", role)
 $("#header").prepend(formattedName);
 $("#header").append(formattedRole);
 
+var bio = {
+  "name":"Robert L. Davis",
+  "email" : <img src = "images/rdavis_BlackArialEmail.png />",
+  "role" : "Senior Business Analyst"
+};
+$("#main").append(bio.name);
+$("#main").append(bio.email);
+
+
