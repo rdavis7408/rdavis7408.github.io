d51ca4bf29bdf5b9b95b7e8bcb7a0bb75aa9355a
rdavis7408 <rdavis7408@gmail.com>
2015-11-16 15:15:35 -0600

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -47,7 +47,7 @@ var HTMLschoolName = "<a href='#'>%data%";
 var HTMLschoolDegree = " - %data%</a>";
 var HTMLschoolDates = "<div class='date-text'>%data%</div>";
 var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
-var HTMLschoolMajor = "<em><br>Major: %data%</em>"
+var HTMLschoolMajor = "<em><br>Concentration: %data%<br></em>"
 var HTMLschoolMinor = "<em><br>Minor: %data%</em>"
 
 var HTMLonlineClasses = "<h3>Online Classes</h3>";
