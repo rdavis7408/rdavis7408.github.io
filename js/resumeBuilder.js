749e933ffbe15d143ec05e0e3e3e4ffc16122f7f
rdavis7408 <rdavis7408@gmail.com>
2015-11-16 14:13:17 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -37,7 +37,10 @@ formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.URL));
 formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
 formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
 
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
-$("#header").append(formattedName);
+$("#header").append(formattedWelcomeMsg);
+
 
 
