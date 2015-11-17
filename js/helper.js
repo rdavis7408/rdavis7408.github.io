526056629958d074ba58c43c749751c958768521
rdavis7408 <rdavis7408@gmail.com>
2015-11-17 09:34:46 -0600

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -115,9 +115,9 @@ function initializeMap() {
 
   // This next line makes `map` a new Google Map JavaScript Object and attaches it to
   // <div id="map">, which is appended as part of an exercise late in the course.
-  var Chicago = {lat: -41.8369, lng: 87.6847};
-  //map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  //var Chicago = {lat: -41.8369, lng: 87.6847};
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  // map = new google.maps.Map(document.querySelector('#map'), mapOptions);
   //map = new google.maps.Map(document.getElementById('map'), {
   //  zoom: 4,
   //  center: Chicago
