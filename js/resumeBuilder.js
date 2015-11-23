df382fd74afa103bd4173912d30979e4d3ea9471
rdavis7408 <rdavis7408@gmail.com>
2015-11-23 14:44:49 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -184,7 +184,8 @@ projects.display = function() {
 
 		if (projects.projects[project].images.length > 0) {
 			for (image in projects.projects[project].images) {
-				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]).replace('#', projects.project[project].image.url);
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				var formattedImage = HTMLprojectImage.replace('#', projects.projects[project].url);
 				$(".project-entry:last").append(formattedImage);
 			        
 				
