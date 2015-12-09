499aca4d21e74db14ad528b68bddc0ea9afefdff
rdavis7408 <rdavis7408@gmail.com>
2015-12-09 14:41:09 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -257,7 +257,7 @@ projects.display = function() {
  		for(i in education.schools) {
  			$("#education").append(HTMLschoolStart);
  
- 			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+ 			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("urllink", education.schools[i].url);
  			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
  			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
  			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);			
