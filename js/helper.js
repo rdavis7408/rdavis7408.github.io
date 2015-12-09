c75171ed945eccacb4b00841c4a6696324b7806b
rdavis7408 <rdavis7408@gmail.com>
2015-12-09 15:04:53 -0600

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -41,7 +41,7 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<a href="#"><img src="%data%"></a>';
 
 var HTMLcertification = '<div class="certification-entry"></div>';
-var HTMLcertificationName = '<br><a href="#"> %data%</a>';
+var HTMLcertificationName = '<a href="#"> %data%</a>';
 var HTMLcertificationOrg = '<div class="certification-org">%data%</div>';
 var HTMLcertificationDate = '<div class="date-text">%date%</div>';
 
