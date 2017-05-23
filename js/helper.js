fa8e8b84131a677549b5de3f2c880ff901ec298f
Robert Davis <rdavis7408@gmail.com>
2017-05-23 12:02:26 -0500

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -24,7 +24,7 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<div id=skillDiv><h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+/*var HTMLskillsStart = '<div id=skillDiv><h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';*/
 /*var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li></div>';*/
 var HTMLsummary = '<div id=summary><h3 style= "color:#BBC4C9">Summary :</h3><br><span class="white-text">%data%<br></span><br></div>';
 
